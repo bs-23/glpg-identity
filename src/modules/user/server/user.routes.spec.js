0d4149e95242682af68719a7549a610fb162677b
LabebaBS23 <labeba.tahsin@bs-23.net>
2020-05-04 20:38:52 +0600

    duplicate email , unauthorized error with invalid access token unit test

--- src/modules/user/server/user.routes.spec.js
+++ src/modules/user/server/user.routes.spec.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
 const app = require(path.join(process.cwd(), "src/config/server/lib/express"))();
 const specHelper = require(path.join(process.cwd(), "src/config/server/jest/spec.helper"));
 
@@ -37,8 +38,8 @@ describe("User Api", () => {
             .post("/users")
             .set("Cookie", [`access_token=${systemAdmin.access_token}`])
             .send({
-                name: "testUser1",
-                email: "testUser1@gmail.com",
+                name: "testUser",
+                email: "testUser@gmail.com",
                 password: "abcpassword",
                 phone: "880111111",
                 type: "Site Admin",
@@ -47,5 +48,43 @@ describe("User Api", () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.res.headers["content-type"]).toMatch("application/json");
+    });
+
+    it("Should get an error when creating new site admin with duplicate email", async () => {
+        const response = await request(app)
+            .post("/users")
+            .set("Cookie", [`access_token=${systemAdmin.access_token}`])
+            .send({
+                name: "testUser",
+                email: "testUser@gmail.com",
+                password: "abcpassword",
+                phone: "880111111",
+                type: "Site Admin",
+                countries: "USA"
+            });
+
+        expect(response.statusCode).toBe(400);
+    });
+
+    it("Should get 401 unauthorized error with invalid access token when creating new site admin", async () => {
+        const invalid_access_token = jwt.sign({
+            id: "f29b63e5-36c7-4210-a5a8-c1e9d0c5b9b8",
+            name: "invalid",
+            email: "invalid@ciam.com",
+        }, process.env.TOKEN_SECRET, { expiresIn: "2d", issuer: "f29b63e5-36c7-4210-a5a8-c1e9d0c5b9b8" })
+
+        const response = await request(app)
+            .post("/users")
+            .set("Cookie", [`access_token=${invalid_access_token}`])
+            .send({
+                name: "testUserNew",
+                email: "testUserNew@gmail.com",
+                password: "abcpassword",
+                phone: "880111111",
+                type: "Site Admin",
+                countries: "USA"
+            });
+
+        expect(response.statusCode).toBe(401);
     });
 });
