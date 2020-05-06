2ab68f6a53b90e428f44819703dd99c8e54ba691
Shibbir Ahmed <shibbir.io@outlook.com>
2020-05-06 15:42:45 +0600

    Add user.routes.js

--- src/modules/core/client/app.component.js
+++ src/modules/core/client/app.component.js
@@ -2,15 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter, Switch, Route } from "react-router-dom";
 
+import "bootstrap/scss/bootstrap";
+import "./app.scss";
+
 import NoMatch from "./NoMatch";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 import Login from "../../user/client/components/login.component";
 import Dashboard from "../../user/client/components/dashboard.component";
 import { getSignedInUserProfile } from "../../user/client/user.actions";
-import UserForm from "../../user/client/components/user-form.component";
-
-import "../../content/scss/main.scss";
+import UserRoutes from "../../user/client/user.routes";
 
 class App extends React.Component {
     constructor(props) {
@@ -25,7 +26,7 @@ class App extends React.Component {
 
                 <PrivateRoute exact path="/" component={Dashboard}/>
 
-                <PrivateRoute exact path="/users/create" component={UserForm}/>
+                <Route path="/users" component={UserRoutes}/>
 
                 <Route component={NoMatch} />
             </Switch>
---
+++ src/modules/user/client/user.routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, useRouteMatch } from "react-router-dom";
+import UserForm from "./components/user-form.component";
+import PrivateRoute from "../../core/client/PrivateRoute";
+
+export default function UserRoutes() {
+    let { path } = useRouteMatch();
+
+    return (
+        <Switch>
+            <PrivateRoute path={`${path}/create`} component={UserForm}/>
+        </Switch>
+    );
+}
