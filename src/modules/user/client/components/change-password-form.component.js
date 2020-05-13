a5b1c1a58b8adc6352b387a3d99e582c6dfa8856
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-05-13 15:57:14 +0600

    Create Change Password Component

---
+++ src/modules/user/client/components/change-password-form.component.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Form, withFormik, Field, ErrorMessage } from 'formik';
+
+import { changePassword } from '../user.actions';
+import { changePasswordSchema } from '../user.schema';
+
+class ChangePasswordForm extends React.Component {
+    render() {
+        const { handleSubmit, isSubmitting } = this.props;
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-5 col-md-8 col-12 my-4 mx-auto p-0 shadow border bg-white">
+                        <div className="p-3 bg-light h5 rounded-top">
+                            Change Password
+                        </div>
+                        <div className="card-body">
+                            <Form onSubmit={handleSubmit}>
+                                <div className="form-group">
+                                    <Field
+                                        className="form-control"
+                                        type="password"
+                                        name="currentPassword"
+                                        placeholder="Current Password"
+                                    />
+                                    <div className="invalid-feedback">
+                                        <ErrorMessage name="currentPassword" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <Field
+                                        className="form-control"
+                                        type="password"
+                                        name="newPassword"
+                                        placeholder="New Password"
+                                    />
+                                    <div className="invalid-feedback">
+                                        <ErrorMessage name="newPassword" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <Field
+                                        className="form-control"
+                                        type="password"
+                                        name="confirmPassword"
+                                        placeholder="Confirm New Password"
+                                        autoComplete="current-password"
+                                    />
+                                    <div className="invalid-feedback">
+                                        <ErrorMessage name="confirmPassword" />
+                                    </div>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-info btn-block"
+                                    disabled={isSubmitting}
+                                >
+                                    Submit
+                                </button>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ChangePasswordForm = withFormik({
+    enableReinitialize: true,
+
+    validationSchema: changePasswordSchema,
+
+    mapPropsToValues: () => ({
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+    }),
+
+    handleSubmit: (values, { setSubmitting, resetForm, props }) => {
+        setSubmitting(false);
+        console.log('Form values: ', values);
+        const formData = {
+            currentPassword: values.currentPassword,
+            newPassword: values.newPassword,
+            confirmPassword: values.confirmPassword,
+        };
+
+        props
+            .changePassword(formData)
+            .then(function() {
+                resetForm();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+
+    displayName: 'ChangePasswordForm',
+})(ChangePasswordForm);
+
+const mapDispatchToProps = dispatch => ({
+    changePassword: formData => dispatch(changePassword(formData)),
+});
+
+export default withRouter(
+    connect(null, mapDispatchToProps)(ChangePasswordForm)
+);
--- src/modules/user/client/user.schema.js
+++ src/modules/user/client/user.schema.js
@@ -1,24 +1,34 @@
-import { string, object } from "yup";
+import { string, object, ref } from 'yup';
 
 export const loginSchema = object().shape({
     email: string()
-        .email("This field should be an valid email address.")
-        .required("This field must not be empty."),
-    password: string()
-        .required("This field must not be empty.")
+        .email('This field should be an valid email address.')
+        .required('This field must not be empty.'),
+    password: string().required('This field must not be empty.'),
 });
 
 export const registerSchema = object().shape({
     name: string()
-        .min(2, "This field must be at least 2 characters long.")
-        .max(20, "This field must be at most 20 characters long.")
-        .required("This field must not be empty."),
+        .min(2, 'This field must be at least 2 characters long.')
+        .max(20, 'This field must be at most 20 characters long.')
+        .required('This field must not be empty.'),
     email: string()
-        .email("This field should be an valid email address.")
-        .required("This field must not be empty."),
+        .email('This field should be an valid email address.')
+        .required('This field must not be empty.'),
     password: string()
-        .min(8, "This field must be at least 8 characters long.")
-        .required("This field must not be empty."),
-    phone: string()
-        .matches(/^[0-9]/, "This field only contains numbers")
+        .min(8, 'This field must be at least 8 characters long.')
+        .required('This field must not be empty.'),
+    phone: string().matches(/^[0-9]/, 'This field only contains numbers'),
+});
+
+export const changePasswordSchema = object().shape({
+    currentPassword: string()
+        .min(8, 'This field must be at least 8 characters long.')
+        .required('This field must not be empty.'),
+    newPassword: string()
+        .min(8, 'This field must be at least 8 characters long.')
+        .required('This field must not be empty.'),
+    confirmPassword: string()
+        .required('This field must not be empty.')
+        .oneOf([ref('newPassword'), null], 'Passwords must match'),
 });
