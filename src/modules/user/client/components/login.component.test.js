4fb06751b281517f36c9e3f829bdacba03e3fdb5
bshabib <Habibur.Rahman@brainstation23.com>
2020-05-13 20:37:46 +0600

    dkfj

---
+++ src/modules/user/client/components/login.component.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+// const React = require('react')
+// import Enzyme, { configure, shallow, mount } from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
+
+// // import Login from "./login.component"
+
+// configure({ adapter: new Adapter() });
+
+
+describe("Login component", () => {
+    it("should render the login component", () => {
+        
+        // const wrapper = shallow(<Login />);
+        
+
+        // expect(wrapper.exists()).toBe(true);
+    })
+})
