fd6e01e08b84f26f9de7fdb051d9736527f04b33
Masud <masud@bs-23.net>
2020-06-24 20:32:42 +0600

    newrelic

---
+++ newrelic.js
@@ -0,0 +1,20 @@
+'use strict'
+
+exports.config = {
+  app_name: ['Customer Data Platform'],
+  license_key: process.env.NEWRELIC_LICENSE_KEY,
+  logging: {
+    level: 'trace',
+    filepath: './newrelic_agent.log'
+  },
+  utilization: {
+    detect_aws: false,
+    detect_pcf: false,
+    detect_azure: false,
+    detect_gcp: false,
+    detect_docker: false
+  },
+  transaction_tracer: {
+    enabled: true
+  }
+}
