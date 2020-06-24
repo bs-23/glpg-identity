724abc175975fe7ad1897c5e0b398a340002831d
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-06-24 13:58:36 +0600

    Fix the problem with test files after the integration of secret manager

--- tests/server/core.routes.spec.js
+++ tests/server/core.routes.spec.js
@@ -1,20 +1,31 @@
 const path = require('path');
-const request = require('supertest');
-const app = require(path.join(process.cwd(), 'src/config/server/lib/express'))();
+const supertest = require('supertest');
+
+const app = require(path.join(process.cwd(), 'src/config/server/lib/express'));
+
 const specHelper = require(path.join(process.cwd(), 'jest/spec.helper'));
+
+let request;
+
+beforeAll(async () => {
+    const appInstance = await app();
+    request = supertest(appInstance);
+});
 
 describe('Core Routes', () => {
     it('Should respond with html if requst is not an ajax', async () => {
-        const response = await request(app).get('/');
+        const response = await request.get('/');
 
         expect(response.statusCode).toBe(200);
         expect(response.res.headers['content-type']).toMatch('text/html');
     });
 
     it('should return all available countries', async () => {
-        const response = await request(app)
+        const response = await request
             .get('/api/countries')
-            .set('Cookie', [`access_token=${specHelper.users.defaultAdmin.access_token}`]);
+            .set('Cookie', [
+                `access_token=${specHelper.users.defaultAdmin.access_token}`,
+            ]);
 
         expect(response.statusCode).toBe(200);
     });
