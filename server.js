a7e4422058a3ebf7fd9ce5ee8996a855430bdf5b
Shibbir Ahmed <shibbir.io@outlook.com>
2020-06-25 19:49:21 +0600

    Enable newrelic on production only

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-
 require('dotenv').config();
-require('newrelic');
-const app = require("./src/config/server/lib/app");
+
+if(process.env.NODE_ENV === 'production') {
+    require('newrelic');
+}
+
+const app = require('./src/config/server/lib/app');
 app.start();
