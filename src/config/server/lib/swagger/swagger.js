b08c59fddb2f8f0c6c2e6bcbfc9349e0d9d6c608
Faisal Amin <faisal.nstu@ymail.com>
2020-06-29 16:17:44 +0600

    swagger upgraded to openapi 3

--- src/config/server/lib/swagger/swagger.js
+++ src/config/server/lib/swagger/swagger.js
@@ -1,23 +1,38 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const options = {
-
-    /**
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: `GLPG CDP APIs`,
+            description: 'API documentation for the GLPG CDP portal',
+            version: '1.0.0',
+        },
+        components: {
+            securitySchemes: {
+                CookieAuth: {
+                    type: 'apiKey',
+                    in: 'cookie',
+                },
+            },
+        },
+        security: [
+            {
+                CookieAuth: [],
+            },
+        ],
+        servers: [
+            {
+                url: 'http://localhost:5050',
+                description: 'Local server',
+            }
+        ],
+    },
+	/**
      * List of files to be processed.
      * Exact file path or globs can be added
      * e.g. './routes/*.js' (for globs)
      */
-    apis: ['./src/config/server/lib/swagger/*.yaml'],
-
-    basePath: '/',
-    swaggerDefinition: {
-        info: {
-            swagger: '2.0',
-            description: 'GLPG CDP portal API Documentation.',
-            title: 'GLPG CDP',
-            version: '1.0.0'
-        },
-        servers: ["http://localhost:5050"]
-    },
+    apis: ['./src/config/server/lib/swagger/*.yaml']
 };
 
 const specs = swaggerJsdoc(options);
