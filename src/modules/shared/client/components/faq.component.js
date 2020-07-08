55a18d011e9fe4fa01a160346b4ed0db3d3307e4
bshabib <Habibur.Rahman@brainstation23.com>
2020-07-08 11:32:43 +0600

    new module shared, created faq component and created information-management component as intermediate page

---
+++ src/modules/shared/client/components/faq.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Faq = () => {
+    return (
+        <React.Fragment>
+            <h4>FAQ</h4>
+            {/* <Accordion defaultActiveKey="0">
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="0" className="text-primary" role="button">
+                    What is the meaning of Lorem ipsum?
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>Literally it does not mean anything. It is a sequence of words without a sense of Latin derivation that make up a text also known as filler text, fictitious, blind or placeholder</Card.Body>
+                </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="1" className="text-primary" role="button">
+                    Why is Lorem Ipsum Dolor used?
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                    <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
+                </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="2" className="text-primary" role="button">
+                        What is the most used version?
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="2">
+                        <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="3" className="text-primary" role="button">
+                        What are the origins of Lorem Ipsum Dolor Sit?
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="3">
+                        <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion> */}
+        </React.Fragment>
+    );
+}
+ 
+export default Faq;
--- src/modules/user/client/components/dashboard.component.js
+++ src/modules/user/client/components/dashboard.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "./dashboard.scss";
+import Faq from '../../../shared/client/components/faq.component';
 
 export default function Dashboard() {
     return (
@@ -34,41 +35,7 @@ export default function Dashboard() {
                     </div>
 
                     <div className="col-12 col-sm-4 col-md-3 p-3 ">
-                        {/*<h4>FAQ</h4>
-                        <Accordion defaultActiveKey="0">
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="0" className="text-primary" role="button">
-                                What is the meaning of Lorem ipsum?
-                            </Accordion.Toggle>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>Literally it does not mean anything. It is a sequence of words without a sense of Latin derivation that make up a text also known as filler text, fictitious, blind or placeholder</Card.Body>
-                            </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="1" className="text-primary" role="button">
-                                Why is Lorem Ipsum Dolor used?
-                            </Accordion.Toggle>
-                            <Accordion.Collapse eventKey="1">
-                                <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
-                            </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="2" className="text-primary" role="button">
-                                    What is the most used version?
-                                </Accordion.Toggle>
-                                <Accordion.Collapse eventKey="2">
-                                    <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="3" className="text-primary" role="button">
-                                    What are the origins of Lorem Ipsum Dolor Sit?
-                                </Accordion.Toggle>
-                                <Accordion.Collapse eventKey="3">
-                                    <Card.Body>The Lorem Ipsum text is used to fill spaces designated to host texts that have not yet been published. They use programmers, graphic designers, typographers to get a real impression of the digital / advertising / editorial product they are working on.</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>*/}
+                        <Faq/>
                     </div>
                 </div>
             </div>
