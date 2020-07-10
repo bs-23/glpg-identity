61f484b0cc00d29666c7dcb7a46fd0608e641791
Masud <masud@bs-23.net>
2020-07-10 12:22:29 +0600

    apm working

--- newrelic.js
+++ newrelic.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const nodecache = require(path.join(process.cwd(), 'src/config/server/lib/nodecache'));
-
+console.log('nr license key ' + process.env.NEW_RELIC_LICENSE_KEY);
 exports.config = {
-    app_name: [process.env.NEW_RELIC_APP_NAME],
-    license_key: nodecache.getValue('NEW_RELIC_LICENSE_KEY'),
+    app_name: ["Customer Data Platform DEV"], // [process.env.NEW_RELIC_APP_NAME],
+    license_key: "eu01xx9b7a5e9042d58cf5e32c79816a5c74NRAL", // process.env.NEW_RELIC_LICENSE_KEY, //nodecache.getValue('NEW_RELIC_LICENSE_KEY'),
     logging: {
         level: 'trace',
        filepath: 'newrelic_agent.log'
     },
+    high_security : false,
     utilization: {
         detect_aws: false,
         detect_pcf: false,
