9df49767986eded297edd6fa766f114e85cf44dc
LabebaBS23 <labeba.tahsin@bs-23.net>
2020-07-14 16:53:38 +0600

    toast component delete

--- src/modules/user/client/components/toast.component.js
+++
@@ -1,8 +0,0 @@
-import { ToastProvider } from 'react-toast-notifications';
-export const Toast = ({ appearance, children }) => (
-
-
-    <div className={{ background: appearance === 'error' ? 'bg-danger' : 'bg-success' }}>
-        {children}
-    </div>
-);
