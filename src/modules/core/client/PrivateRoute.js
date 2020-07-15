4836f2450cc261022d6e11315c6d1286b515b9fe
bshabib <Habibur.Rahman@brainstation23.com>
2020-07-15 19:02:27 +0600

    modified

--- src/modules/core/client/PrivateRoute.js
+++ src/modules/core/client/PrivateRoute.js
@@ -2,20 +2,16 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 import Navbar from "./components/navbar.component";
-import { useToasts } from 'react-toast-notifications';
+
 
 export default function PrivateRoute({ component: Component, module, ...rest }) {
-    const { addToast } = useToasts();
-
     const loggedInUser = useSelector(state => state.userReducer.loggedInUser);
-    let permissions = [];
-    if(loggedInUser) permissions = loggedInUser.permissions
+    const permissions = loggedInUser.permissions || []
 
     return (
-        <Route {...rest} render={ props => {
-            
+        <Route {...rest} render={props => {
             return (
-                loggedInUser ? (!module || permissions.some( module_name => module_name === module)) ? (
+                loggedInUser ? (!module || permissions.some( module_permission => module_permission === module)) ? (
                     <> 
                         <Navbar/>
                         <Component {...props} />
