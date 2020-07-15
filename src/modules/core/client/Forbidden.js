5caf5c1328cd78c51bdd6a368cfd1bd11e87ffe5
tasnim <tasnim.rahman@bs-23.com>
2020-07-15 22:09:26 +0600

    add button and style

--- src/modules/core/client/Forbidden.js
+++ src/modules/core/client/Forbidden.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 export default function NoMatch() {
     return (
-        <div className="h4">Forbidden! You are not authorized to view this page</div>
+        <div>
+            <div className="alert alert-danger col-md-5 col-md-offset-5"><strong>Forbidden!</strong> You are not authorized to view this page</div>
+            <NavLink to="/">Back to Dashboard</NavLink>
+        </div>
+
     );
 }
