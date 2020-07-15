885e2fbc95a4a6e57aa9b9836db5e958575ac9ce
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-07-15 11:22:29 +0600

    Fix Users.component tests after update

--- tests/client/users.component.spec.js
+++ tests/client/users.component.spec.js
@@ -21,28 +21,42 @@ beforeAll(async () => {
     await store.dispatch(getSignedInUserProfile())
 });
 
-describe('User component', () => {
-    beforeEach(() => {
-        mockAxios = new MockAdapter(axios);
-    });
+let data
+beforeEach(() => {
+    mockAxios = new MockAdapter(axios);
+    const countries = [{ countryid: 0, countryname: 'null' }, { countryid: 1, countryname: 'Ireland' }, { countryid: 2, countryname: 'Netherlands' }]
+    const limit = 3
+    data = {
+        users: [
+            { id: '1', name: 'John', email: 'email@gmail.com', country: 'Ireland' },
+            { id: '2', name: 'Smith', email: 'email2@gmail.com', country: 'Netherlands' },
+            { id: '3', name: 'Carl', email: 'email3@gmail.com', country: 'Luxembourg' },
+            { id: '4', name: 'Johnson', email: 'email4@gmail.com', country: 'Netherlands' },
+            { id: '5', name: 'Brandon', email: 'email5@gmail.com', country: 'Netherlands' },
+        ]
+    }
 
+    mockAxios.onGet('/api/countries').reply(200, countries)
+    mockAxios.onDelete(`/api/users/1`).reply(200, { id: '1' })
+    countries.forEach(country => {
+        if(country.countryname === 'null') return
+        const response = {...data}
+        const { users: allUsers } = response
+        response.users = allUsers.filter(user => user.country === country.countryname)
+        mockAxios.onGet(`/api/users?page=${1}&country=${country.countryname}`).reply(200, response)
+    })
+    mockAxios.onGet(`/api/users?page=${1}&country=null`).reply(200, { users: data.users.slice(0, limit), page: 1,  end: 3 })
+    mockAxios.onGet(`/api/users?page=${2}&country=null`).reply(200, { users: data.users.slice(limit), page: 2,  end: 5 })
+})
+
+describe('Users component', () => {
     it('Should get signed in profile', () => {
         const signedInProfile = store.getState().userReducer.loggedInUser
         expect(signedInProfile).toBeTruthy()
     })
 
     it('Should render user table and delete one user', async () => {
-        const users = {
-            users: [
-                { id: '1', name: 'John', email: 'email@gmail.com' },
-                { id: '2', name: 'Smith', email: 'anothermail@gmail.com' }
-            ]
-        }
-
-        mockAxios.onGet(`/api/users?page=${1}&country=${null}`).reply(200, users)
-        mockAxios.onDelete(`/api/users/1`).reply(200, { id: '1' })
-
-        const { container } = render(
+        const { container, queryByText } = render(
             <Provider store={store}>
                 <MemoryRouter>
                     <ToastProvider>
@@ -56,8 +70,8 @@ describe('User component', () => {
         await waitFor(() => {
             table = container.querySelector('table')
             expect(table).toBeTruthy()
+            expect(queryByText('John')).toBeTruthy()
         })
-
 
         const tbody = container.querySelector('tbody')
         const row1 = tbody.firstChild
@@ -65,33 +79,17 @@ describe('User component', () => {
 
         window.confirm = jest.fn(() => true)
 
-        users.users  = users.users.filter(e => e.id === '1')
+        data.users = data.users.filter(user => user.name !== 'John')
+        mockAxios.onGet(`/api/users?page=${1}&country=null`).reply(200, data)
+
         fireEvent.click(row1Delete)
 
         await waitFor(() => {
-            expect(tbody.childNodes.length).toBe(1)
+            expect(queryByText('John')).toBeFalsy()
         })
     });
 
     it('Should filter users based on country', async () => {
-        const conuntries = ["null", "Ireland","Netherlands","Luxembourg"]
-
-        const data = {
-            users: [
-                { id: '1', name: 'John', email: 'email@gmail.com', country: 'Ireland' },
-                { id: '2', name: 'Smith', email: 'email2@gmail.com', country: 'Netherlands' },
-                { id: '3', name: 'Carl', email: 'email3@gmail.com', country: 'Luxembourg' },
-                { id: '4', name: 'Johnson', email: 'email4@gmail.com', country: 'Luxembourg' }
-            ]
-        }
-
-        conuntries.forEach(country => {
-            const response = {...data}
-            const { users: allUsers } = response
-            if(country !== 'null') response.users = allUsers.filter(user => user.country === country)
-            mockAxios.onGet(`/api/users?page=${1}&country=${country}`).reply(200, response)
-        })
-
         const { container, getByText } = render(
             <Provider store={store}>
                 <MemoryRouter>
@@ -109,34 +107,20 @@ describe('User component', () => {
         })
 
         const tbody = container.querySelector('tbody')
-        expect(tbody.childNodes.length).toBe(4)
+        expect(tbody.childNodes.length).toBe(3)
 
         const filter_button = getByText('Filter')
         fireEvent.click(filter_button)
 
-        const selected_country = getByText('Luxembourg')
-        fireEvent.click(selected_country)
+        const country_label = getByText('Ireland')
+        fireEvent.click(country_label)
 
         await waitFor(() => {
-            expect(tbody.childNodes.length).toBe(2)
+            expect(tbody.childNodes.length).toBe(1)
         })
     });
 
-    it('Should filter users based on country', async () => {
-        const limit = 2
-        const data = {
-            users: [
-                { id: '1', name: 'John', email: 'email@gmail.com', country: 'Ireland' },
-                { id: '2', name: 'Smith', email: 'email2@gmail.com', country: 'Netherlands' },
-                { id: '3', name: 'Carl', email: 'email3@gmail.com', country: 'Luxembourg' },
-            ],
-            total: 3
-        }
-        const { users } = data
-
-        mockAxios.onGet(`/api/users?page=${1}&country=null`).reply(200, { users: users.slice(0, limit), page: 1,  end: 2 })
-        mockAxios.onGet(`/api/users?page=${2}&country=null`).reply(200, { users: users.slice(limit), page: 2,  end: 3 })
-
+    it('Should go to the next page', async () => {
         const { container, getByText, queryByText } = render(
             <Provider store={store}>
                 <MemoryRouter>
@@ -154,24 +138,24 @@ describe('User component', () => {
         })
 
         const tbody = container.querySelector('tbody')
-        expect(tbody.childNodes.length).toBe(2)
+        expect(tbody.childNodes.length).toBe(3)
 
         const next_button = getByText('Next')
         fireEvent.click(next_button)
 
         await waitFor(() => {
-            expect(queryByText('Carl')).toBeTruthy()
+            expect(queryByText('Johnson')).toBeTruthy()
             expect(queryByText('John')).toBeFalsy()
-            expect(tbody.childNodes.length).toBe(1)
+            expect(tbody.childNodes.length).toBe(2)
         })
 
         const prev_button = getByText('Prev')
         fireEvent.click(prev_button)
 
         await waitFor(() => {
-            expect(queryByText('Carl')).toBeFalsy()
+            expect(queryByText('Johnson')).toBeFalsy()
             expect(queryByText('John')).toBeTruthy()
-            expect(tbody.childNodes.length).toBe(2)
+            expect(tbody.childNodes.length).toBe(3)
         })
     });
 });
