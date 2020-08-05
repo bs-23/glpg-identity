9fd33f201fee2f469358f3b1774b56418db2d2f4
bshabib <Habibur.Rahman@brainstation23.com>
2020-08-05 15:10:45 +0600

    bug solved for cdp users first name last name to contain only characters

--- src/modules/user/client/user.schema.js
+++ src/modules/user/client/user.schema.js
@@ -32,10 +32,12 @@ export const loginSchema = object().shape({
 
 export const registerSchema = object().shape({
     first_name: string()
+        .matches(/^[a-zA-Z]+$/, 'This field only contains letters')
         .min(2, 'This field must be at least 2 characters long.')
         .max(20, 'This field must be at most 20 characters long.')
         .required('This field must not be empty.'),
     last_name: string()
+        .matches(/^[a-zA-Z]+$/, 'This field only contains letters')
         .min(2, 'This field must be at least 2 characters long.')
         .max(20, 'This field must be at most 20 characters long.')
         .required('This field must not be empty.'),
