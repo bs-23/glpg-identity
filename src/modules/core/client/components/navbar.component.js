2b880c893858e3be7795741545e792007ed8d42a
Ruhul Amin <ruhul100@gmail.com>
2020-08-07 21:04:25 +0600

    application and country selector styling

--- src/modules/core/client/components/navbar.component.js
+++ src/modules/core/client/components/navbar.component.js
@@ -38,24 +38,15 @@ export default function Navbar() {
                         </div>
                     </div>
                     <div className="col-9 text-right">
-                        <div className="d-flex justify-content-end">
-                            <Dropdown className="dropdown-customize">
-                                <Dropdown.Toggle id="applications" className="btn cdp-btn-outline-gray cdp-text-primary mr-2 py-1 px-2 d-flex align-items-center">
-                                    <i className="icon icon-applications mr-1 dropdown-customize__icon-with-bg"></i> Applications
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-1">Brand X</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Dropdown className="dropdown-customize">
-                                <Dropdown.Toggle id="countries" className="btn cdp-btn-outline-gray cdp-text-primary mr-2  py-1 px-2 d-flex align-items-center">
-                                    <i className="icon icon-europe-map mr-1 dropdown-customize__icon-with-bg"></i> Countries
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-1"><img height="26" width="26" src="/assets/flag/flag-netherlands.svg" alt="Flag" /> Netherlands</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-1"><img height="26" width="26" src="/assets/flag/flag-belgium.svg" alt="Flag" /> Belgium</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
+                        <div className="d-flex justify-content-end align-items-center">
+                            <div className="mr-3">
+                                <img src="https://cdp-asset.s3.eu-central-1.amazonaws.com/brandx_logo.png" title="Brand X" alt="Brand X Logo" width="122" />
+                            </div>
+                            <div className="mr-2">
+                                <img height="26" width="26" src="/assets/flag/flag-netherlands.svg" title="Netherlands" alt="Flag" className="ml-1" />
+                                <img height="26" width="26" src="/assets/flag/flag-belgium.svg" title="Belgium" alt="Flag" className="ml-1" />
+                                <img height="26" width="26" src="/assets/flag/flag-france.svg" title="France" alt="Flag" className="ml-1" />
+                            </div>
                             <button className="mr-2 btn cdp-btn-secondary text-white"><i className="icon icon-user-round mr-1 app__header-icon-user d-none d-sm-inline-block"></i> <span className="">{first_name + " " + last_name}</span></button>
                             <a className="btn cdp-btn-outline-primary d-flex align-items-center" onClick={handleLogOut} href="/api/logout"><i className="icon icon-logout mr-1 app__header-icon-logout"></i>Sign out</a>
                         </div>
