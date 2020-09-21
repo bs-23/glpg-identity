f17c4e6ac4b477ba5d2d1b4672fd07fc95a8048c
Ruhul Amin <ruhul100@gmail.com>
2020-09-21 12:45:24 +0600

    Icon change for consent confirmation

--- src/modules/hcp/client/components/hcp-users.js
+++ src/modules/hcp/client/components/hcp-users.js
@@ -201,7 +201,7 @@ export default function hcpUsers() {
                                                                         key={authority.name} className="border-bottom"
                                                                         href={authority.link}
                                                                         target="_blank">
-                                                                        <img src={authority.logo} title={authority.name + " Logo"} alt={authority.name} height="30" />
+                                                                        <img src={authority.logo} title={authority.name + " Logo"} alt={authority.name} height="25" />
                                                                     </Dropdown.Item>
                                                                 )
                                                             )
@@ -456,8 +456,8 @@ export default function hcpUsers() {
                                                         <td>{row.uuid}</td>
                                                         <td><span>{getCountryName(row.country_iso2)}</span></td>
                                                         <td>{row.specialty_description}</td>
-                                                        <td>{row.opt_types.includes('single-opt-in') ? <i className="icon icon-check-filled cdp-text-primary"></i> : <i className="icon icon-close-circle text-danger consent-not-given"> </i>}</td>
-                                                        <td>{row.opt_types.includes('double-opt-in') ? <i className="icon icon-check-filled cdp-text-primary"></i> : <i className="icon icon-close-circle text-danger consent-not-given"> </i>}</td>
+                                                        <td>{row.opt_types.includes('single-opt-in') ? <i className="fas fa-check-circle cdp-text-primary font-size-15px"></i> : <i className="icon icon-close-circle text-danger consent-not-given"> </i>}</td>
+                                                        <td>{row.opt_types.includes('double-opt-in') ? <i className="fas fa-check-circle cdp-text-primary font-size-15px"></i> : <i className="icon icon-close-circle text-danger consent-not-given"> </i>}</td>
                                                         <td>
                                                             <span>
                                                                 <Dropdown className="ml-auto dropdown-customize">
