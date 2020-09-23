78db692a40e693e0e32d67c63c38a28fb255c604
bshabib <Habibur.Rahman@brainstation23.com>
2020-09-23 20:11:07 +0600

    name issue fixed from consent performance report

--- src/modules/consent/client/components/veeva-consent-performance-report.component.js
+++ src/modules/consent/client/components/veeva-consent-performance-report.component.js
@@ -265,7 +265,7 @@ const ConsentPerformanceReport = () => {
                                     <div className="px-4 py-3">
                                         <div className="row">
                                             <div className="col">
-                                                <h4 className="mt-1 font-weight-bold">{`${currentUser.first_name || ''} ${currentUser.last_name || ''}`}</h4>
+                                                <h4 className="mt-1 font-weight-bold">{`${currentUser.name || ''}`}</h4>
                                                 <div className="">{currentUser.specialty_description}</div>
                                             </div>
                                         </div>
