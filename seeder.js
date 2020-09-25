2b19c7b4f8f49814dbcf5bfaff2ee4975ae1f9f8
Shibbir Ahmed <shibbir.io@outlook.com>
2020-09-25 21:18:16 +0600

    Update links in consents

--- seeder.js
+++ seeder.js
@@ -236,7 +236,7 @@ async function init() {
                 locale: 'nl_be'
             },
             {
-                rich_text: "<p>Ik bevestig dat ik een professionele zorgverlener ben die in Nederland werkzaam is en ik accepteer de <a href='https://www.glpg.com/nederland/disclaimer' target='_blank'>Gebruiksvoorwaarden</a> van Galapagos.nl.</p>",
+                rich_text: "<p>Ik bevestig dat ik een professionele zorgverlener ben die in Nederland werkzaam is en ik accepteer de <a href='https://www.jyseleca.nl/nl/static-pages/terms-of-use.html' target='_blank'>Gebruiksvoorwaarden</a> van Galapagos.nl.</p>",
                 consent_id: 'ebea072a-81d4-4507-a46b-cb365ea0c6db',
                 locale: 'nl_nl'
             },
@@ -246,7 +246,7 @@ async function init() {
                 locale: 'nl_nl'
             },
             {
-                rich_text: "<p>Ik ontvang graag informatieve en promotionele communicatie via e-mail over de producten, diensten en andere ontwikkelingen van Galapagos. <br> <br> Raadpleeg onze <a href='https://www.glpg.com/nederland/privacyverklaring' target='_blank'>privacyverklaring</a> voor meer informatie over hoe we met uw persoonsgegevens omgaan.</p>",
+                rich_text: "<p>Ik ontvang graag informatieve en promotionele communicatie via e-mail over de producten, diensten en andere ontwikkelingen van Galapagos. <br> <br> Raadpleeg onze <a href='https://www.jyseleca.nl/nl/static-pages/privacy-policy.html' target='_blank'>privacyverklaring</a> voor meer informatie over hoe we met uw persoonsgegevens omgaan.</p>",
                 consent_id: '01cfab4f-9fdd-4975-9a90-bbde78785109',
                 locale: 'nl_nl'
             }
