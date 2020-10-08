8fef9047b36c51093599b1a5eeb6b1fe2570d0bc
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-10-08 13:35:58 +0600

    Add first name last name validation to my profile

--- src/modules/user/client/user.schema.js
+++ src/modules/user/client/user.schema.js
@@ -115,8 +115,15 @@ export const roleSchema = object().shape({
 
 export const updateMyProfileSchema = object().shape({
     first_name: string()
+        .matches(XRegExp('^[\\pL]+(?:\\s[\\pL]+)*$'), 'This field only contains letters')
+        .min(2, 'This field must be at least 2 characters long.')
+        .max(20, 'This field must be at most 20 characters long.')
         .required('This field must not be empty.'),
     last_name: string()
+        .matches(XRegExp('^[\\pL]+(?:\\s[\\pL]+)*$'), 'This field only contains letters')
+        // .matches(XRegExp('^\\pL+$'), 'This field only contains letters')
+        .min(2, 'This field must be at least 2 characters long.')
+        .max(20, 'This field must be at most 20 characters long.')
         .required('This field must not be empty.'),
     email: string()
         .email('This field should be a valid email address.')
