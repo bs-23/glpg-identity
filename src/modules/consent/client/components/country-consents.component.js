00ae1b51835123fb76f7b1548d36a39c2a0cc438
Faisal Amin <faisal.nstu@ymail.com>
2020-10-14 14:26:23 +0600

    country consent component with breadcrumb

--- src/modules/consent/client/components/country-consents.component.js
+++ src/modules/consent/client/components/country-consents.component.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const CdpConsents = () => {
+const CountryConsents = () => {
     return (
         <main className="app__content cdp-light-bg h-100">
             <div className="container-fluid">
-                <div className="row">
+            <div className="row">
                     <div className="col-12 px-0">
                         <nav aria-label="breadcrumb">
                             <ol className="breadcrumb rounded-0">
                                 <li className="breadcrumb-item"><NavLink to="/">Dashboard</NavLink></li>
                                 <li className="breadcrumb-item"><NavLink to="/consent">Data Privacy & Consent Management</NavLink></li>
-                                <li className="breadcrumb-item active"><span>CDP Consents</span></li>
+                                <li className="breadcrumb-item active"><span>Country Consents</span></li>
                             </ol>
                         </nav>
                     </div>
                 </div>
-                <h1>CDP Consents</h1>
+                <h1>Country Consents</h1>
             </div>
         </main>
     );
 }
 
-export default CdpConsents;
+export default CountryConsents;
