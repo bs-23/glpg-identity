bde23c7d33b421fe9546f00122e000778b589f12
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-10-23 13:30:57 +0600

    Add Draft editor wrapper package

--- src/modules/core/client/components/draft-editor.js
+++ src/modules/core/client/components/draft-editor.js
@@ -1,101 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import {Editor, ContentState, EditorState, RichUtils, CompositeDecorator, convertFromHTML} from 'draft-js';
+import React, { useEffect } from 'react';
+import {ContentState, EditorState, convertFromHTML} from 'draft-js';
 import {stateToHTML} from 'draft-js-export-html';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import 'draft-js/dist/Draft.css';
 
-const styles = {
-    link: {
-      color: 'rgb(69, 153, 213)',
-      cursor: 'pointer'
-    },
-};
-
-const Link = (props) => {
-    const {url} = props.contentState.getEntity(props.entityKey).getData();
-    return (
-      <a href={url} style={styles.link}>
-        {props.children}
-      </a>
-    );
-};
-
-function findLinkEntities(contentBlock, callback, contentState) {
-    contentBlock.findEntityRanges(
-      (character) => {
-        const entityKey = character.getEntity();
-        return (
-          entityKey !== null &&
-          contentState.getEntity(entityKey).getType() === 'LINK'
-        );
-      },
-      callback
-    );
-}
-
-const decorator = new CompositeDecorator([
-    {
-      strategy: findLinkEntities,
-      component: Link,
-    },
-]);
-
 export default function DraftEditor({ onChangeHTML, htmlContent }) {
-    const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty(decorator));
-    const [showAddUrlBar, setShowAddUrlBar] = useState(false);
-    const [urlInputValue, setUrlInputValue] = useState('http://');
-
-    const handleKeyCommand = (command, editorState) => {
-        const newState = RichUtils.handleKeyCommand(editorState, command);
-        if (newState) {
-          setEditorState(newState);
-          return 'handled';
-        }
-        return 'not-handled';
-    }
-
-    const handleURLInputChange = (e) => {
-        setUrlInputValue(e.target.value);
-    }
+    const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
 
     const convertContentToHtml = () => {
         const editorContentInHTML = stateToHTML(editorState.getCurrentContent());
         return editorContentInHTML;
-    }
-
-    const handleAddUrl = () => {
-        const contentState = editorState.getCurrentContent();
-        const contentStateWithEntity = contentState.createEntity('LINK', 'SEGMENTED', { url: urlInputValue });
-        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-        const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
-
-        setEditorState(RichUtils.toggleLink(
-            newEditorState,
-            newEditorState.getSelection(),
-            entityKey
-        ));
-
-        setShowAddUrlBar(false);
-        setUrlInputValue('http://');
-    }
-
-    const onBoldClick = () => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-    }
-
-    const onUnderlineClick = () => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
-    }
-
-    const onItalicClick = () => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, 'ITALIC'));
-    }
-
-    const onAddUrlClick = () => {
-        setShowAddUrlBar(true);
-    }
-
-    const onHeaderStyleClick = (style) => {
-        setEditorState(RichUtils.toggleBlockType(editorState, style));
     }
 
     useEffect(() => {
@@ -112,36 +27,12 @@ export default function DraftEditor({ onChangeHTML, htmlContent }) {
                 blocksFromHTML.contentBlocks,
                 blocksFromHTML.entityMap,
             );
-            setEditorState(EditorState.createWithContent(state, decorator));
+            setEditorState(EditorState.createWithContent(state,));
         }
     }, []);
 
-    return <div className="border border-secondary">
-        <div className="p-2" style={{ background: '#d6d6d6' }}>
-            <span className="btn btn-info btn-sm mr-1" onClick={() => onHeaderStyleClick('header-one')}>H1</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={() => onHeaderStyleClick('header-two')}>H2</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={() => onHeaderStyleClick('header-three')}>H3</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={() => onHeaderStyleClick('header-four')}>H4</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={onBoldClick}>Bold</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={onUnderlineClick}>Underline</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={onItalicClick}>Italic</span>
-            <span className="btn btn-info btn-sm mr-1" onClick={onAddUrlClick}>Add Url</span>
-        </div>
-
-        {showAddUrlBar && <div class="input-group">
-            <input type="text" class="form-control" value={urlInputValue} onChange={e => handleURLInputChange(e)}/>
-            <div class="input-group-append">
-                <span class="btn btn-outline-secondary" type="span" onClick={handleAddUrl}>Add Url</span>
-            </div>
-        </div>}
-
-        <div className="p-2">
-            <Editor
-                decorator={decorator}
+    return <Editor
                 editorState={editorState}
-                onChange={setEditorState}
-                handleKeyCommand={handleKeyCommand}
+                onEditorStateChange={setEditorState}
             />
-        </div>
-    </div>;
 }
