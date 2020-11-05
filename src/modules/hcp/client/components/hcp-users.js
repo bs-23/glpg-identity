c7e69b1823354f612c7d5fe0cf76f2e9c77de1e7
Ruhul Amin <ruhul100@gmail.com>
2020-11-05 20:39:14 +0600

    Save button sticky

--- src/modules/hcp/client/components/hcp-users.js
+++ src/modules/hcp/client/components/hcp-users.js
@@ -647,7 +647,7 @@ export default function hcpUsers() {
                                     (editableTableProps) => {
                                         const { dirty, values, touched, status, resetForm, initialValues, submitForm } = editableTableProps;
                                         console.log('current val: ', values.rows)
-                                        return dirty && <div className="cdp-bg-primary text-center p-2">
+                                        return dirty && <div className="cdp-bg-primary text-center p-2 cdp-table-inline-editing">
                                             <div>
                                                 <button className="btn cdp-btn-outline-secondary btn-sm text-white" onClick={resetForm}><i class="fas fa-times-circle mr-1"></i> Reset</button>
                                                 <button className="btn cdp-btn-secondary ml-2 btn-sm text-white" onClick={submitForm} disabled={!dirty}><i class="fas fa-check-circle mr-1"></i>Save Changes</button>
@@ -660,7 +660,7 @@ export default function hcpUsers() {
 
                             {hcps['users'] && hcps['users'].length === 0 &&
                                 <>
-                                    <div className="row justify-content-center mt-sm-5 pt-5 mb-3">
+                                <div className="row justify-content-center mt-sm-5 pt-5 mb-3 cdp-table-inline-editing__save-btn">
                                         <div className="col-12 col-sm-6 py-4 bg-white shadow-sm rounded text-center">
                                             <i class="icon icon-team icon-6x cdp-text-secondary"></i>
                                             <h3 className="font-weight-bold cdp-text-primary pt-4">No Profile Found!</h3>
