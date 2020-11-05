ba03b7ea6c5a8042cc8ec3ea35efc4877762fef3
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-11-05 16:52:04 +0600

    Added width and classname

--- src/modules/core/client/components/EditableTable/Header.js
+++ src/modules/core/client/components/EditableTable/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Header = ({ columns, dirty }) => {
-
     return <thead className="cdp-bg-primary text-white cdp-table__header">
         <tr>
-            {columns && columns.map(col => <th key={col.name} >
+            {columns && columns.map(col => <th width={col.width} className={col.class} key={col.name} >
                 <span>{col.name}</span>
                 {col.onSort && !dirty && <i onClick={col.onSort} className="ml-2 icon icon-history icon-1x cdp-list-group__icon"></i>}
             </th>)}
