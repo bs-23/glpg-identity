2c0ecc86d75433fcf0ba85bb6b5689cb200ff98c
Shibbir Ahmed <shibbir.io@outlook.com>
2020-11-10 13:59:21 +0600

    Set a max length of 50 characters for consent category title

--- src/modules/consent/client/consent-category/category.schema.js
+++ src/modules/consent/client/consent-category/category.schema.js
@@ -3,6 +3,6 @@ import { string, object } from 'yup';
 export default object().shape({
     title: string()
         .min(2, 'This field must be at least 2 characters long.')
-        .max(255, 'This field must be at most 255 characters long.')
+        .max(50, 'This field must be at most 255 characters long.')
         .required('This field must not be empty.')
 });
