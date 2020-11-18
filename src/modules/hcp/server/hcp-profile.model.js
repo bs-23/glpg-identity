1c2977b1a13bee7dadf0fb57497a1b89f506dcfe
Faisal Amin <faisal.nstu@ymail.com>
2020-11-18 16:39:29 +0600

    origin url non nullable

--- src/modules/hcp/server/hcp-profile.model.js
+++ src/modules/hcp/server/hcp-profile.model.js
@@ -97,7 +97,7 @@ const HcpProfile = sequelize.cdpConnector.define('hcp_profiles', {
         type: DataTypes.STRING
     },
     origin_url: {
-        allowNull: true,
+        allowNull: false,
         type: DataTypes.STRING
     }
 }, {
