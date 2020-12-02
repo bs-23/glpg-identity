1000d2fc916264ecec2972f902c031af6b170c65
Shibbir Ahmed <shibbir.io@outlook.com>
2020-12-02 13:35:55 +0600

    Update packages

--- src/config/client/webpack.common.js
+++ src/config/client/webpack.common.js
@@ -49,9 +49,7 @@ module.exports = {
             {
                 test: /\.(scss|css)$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
                 ]
