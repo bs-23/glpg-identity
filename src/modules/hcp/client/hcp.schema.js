4419839b505239735ecb9a94ea2348cca51b988a
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2020-12-02 14:54:45 +0600

    Fix hcp reject related issues

--- src/modules/hcp/client/hcp.schema.js
+++ src/modules/hcp/client/hcp.schema.js
@@ -2,9 +2,15 @@ import { string, object } from 'yup';
 
 
 export const ApprovalRejectSchema = object().shape({
-    comment: string()
-        .required('Must select one option.')
-        .max(500, 'This field must be at most 500 characters long.'),
+    comment: string().when('selectedStatus', {
+        is: 'approve',
+        then: string()
+            .required('Must select one option.')
+            .max(500, 'This field must be at most 500 characters long.'),
+        otherwise: string()
+            .required('This field must not be empty.')
+            .max(500, 'This field must be at most 500 characters long.'),
+    }),
     other_comment: string().when('selectedStatus', {
         is: 'approve',
         then: string().when('comment', {
