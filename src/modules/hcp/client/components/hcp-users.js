fc23e2e3c19f662f757425aa8989c187eceec092
Ruhul Amin <ruhul100@gmail.com>
2020-12-04 16:34:06 +0600

    Opt out icon styling issue fix

--- src/modules/hcp/client/components/hcp-users.js
+++ src/modules/hcp/client/components/hcp-users.js
@@ -276,8 +276,8 @@ export default function hcpUsers() {
         const allOptTypes = ['single-opt-in', 'double-opt-in', 'opt-out'];
         return <div className="text-center ml-n2">
             {value.includes('single-opt-in') ? <i title="Single Opt-In" className="fas fa-check cdp-text-primary mr-3"></i> : ''}
-            {value.includes('double-opt-in') ? <i title="Double Opt-In" className="fas fa-check-double cdp-text-primary"></i> : ''}
-            {value.includes('opt-out') ? <i title="Opt-out" className="far fa-window-close text-danger mr-1"></i> : ''}
+            {value.includes('double-opt-in') ? <i title="Double Opt-In" className="fas fa-check-double cdp-text-primary mr-3"></i> : ''}
+            {value.includes('opt-out') ? <i title="Opt-out" className="far fa-window-close text-danger"></i> : ''}
             {value.filter(val => allOptTypes.some(ot => ot === val)).length ? '' : <div>N/A</div>}
         </div>
     }
