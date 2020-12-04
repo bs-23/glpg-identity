73130ee5f768371dcd7ef66e781ac24f785ec67f
Ruhul Amin <ruhul100@gmail.com>
2020-12-04 12:10:50 +0600

    Help cards styling

--- src/modules/core/client/components/help.component.js
+++ src/modules/core/client/components/help.component.js
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import parse from 'html-react-parser';
 import { useSelector, useDispatch } from 'react-redux';
 import { getFaqItems } from '../../../platform/faq/client/faq.actions';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
 
 export default function Help() {
     const faqData = useSelector(state => state.faqReducer.faq_items);
@@ -15,23 +16,51 @@ export default function Help() {
 
     return (
         <React.Fragment>
-                <h4 className="faq__header p-3 font-weight-bold mb-0 d-flex justify-content-between">
-                    CDP Help Center
-                </h4>
+            <main className="app__content cdp-light-bg h-100">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-12 px-0">
+                            <nav aria-label="breadcrumb">
+                                <ol className="breadcrumb rounded-0">
+                                    <li className="breadcrumb-item"><NavLink to="/">Dashboard</NavLink></li>
+                                    <li className="breadcrumb-item active"><span>Help</span></li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="d-sm-flex justify-content-between align-items-center">
+                                <h4 className="cdp-text-primary font-weight-bold py-3">
+                                    CDP Help Center
+                                </h4>
+                            </div>
+                            <div className="help-center">
+                                <div className="help-center__card mb-3 shadow-sm bg-white faq">
+                                    <h6 className="cdp-bg-primary-lighter text-white mb-0 px-3 py-2">Dashboard</h6>
+                                    <Accordion defaultActiveKey="0" className="faq__body">
+                                        {faqData.faq && faqData.faq.map((faq, index) => (
+                                            <Card key={index}>
+                                                <Accordion.Collapse eventKey={index + ""}>
+                                                    <Card.Body>{parse(faq.answer)}</Card.Body>
+                                                </Accordion.Collapse>
+                                                <Accordion.Toggle as={Card.Header} eventKey={index + ""} className="px-3 py-2 d-flex align-items-baseline justify-content-between" role="button">
+                                                    <span className="faq__question">{faq.question}</span>
+                                                    <i className="icon icon-arrow-down ml-2 faq__icon-down"></i>
+                                                </Accordion.Toggle>
+                                            </Card>
+                                        ))}
+                                    </Accordion>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </main>
+               
 
-                <Accordion defaultActiveKey="0" className="faq__body">
-                    {faqData.faq && faqData.faq.map((faq, index) => (
-                        <Card key={index}>
-                            <Accordion.Collapse eventKey={index + ""}>
-                                <Card.Body>{parse(faq.answer)}</Card.Body>
-                            </Accordion.Collapse>
-                            <Accordion.Toggle as={Card.Header} eventKey={index + ""} className="p-3 d-flex align-items-baseline justify-content-between" role="button">
-                                <span className="faq__question">{faq.question}</span>
-                                <i className="icon icon-arrow-down ml-2 faq__icon-down"></i>
-                            </Accordion.Toggle>
-                        </Card>
-                    ))}
-                </Accordion>
+                
 
                 {faqData.faq && faqData.faq.length === 0 &&
                     <div className="bg-white text-center py-3 px-2 border-top">
