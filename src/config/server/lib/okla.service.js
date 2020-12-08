dfe54b96df6588212a84da0028503c0363e1df89
Faisal Amin <faisal.nstu@ymail.com>
2020-12-08 15:48:13 +0600

    okla search url refactor

--- src/config/server/lib/okla.service.js
+++ src/config/server/lib/okla.service.js
@@ -2,15 +2,14 @@ const path = require('path');
 const axios = require('axios');
 const nodecache = require(path.join(process.cwd(), 'src/config/server/lib/nodecache'));
 
-const baseUrl = nodecache.getValue('OKLA_BASE_URL');
-const cid = nodecache.getValue('OLA_CID');
+const searchUrl = nodecache.getValue('OKLA_SEARCH_URL');
 const auth = {
     username: nodecache.getValue('OKLA_USERNAME'),
     password: nodecache.getValue('OLA_PASSWORD')
 };
 
 async function search(queryObj) {
-    const { data } = await axios.post(`${baseUrl}/ok/search/${cid}`, queryObj, { auth });
+    const { data } = await axios.post(searchUrl, queryObj, { auth });
     return data;
 }
 
