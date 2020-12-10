dc02605aba8a16a83e661534f019dc1b217caf63
bshabib <Habibur.Rahman@brainstation23.com>
2020-12-10 12:49:14 +0600

    reset function updated

--- src/modules/hcp/client/components/search-professional-hcp.component.js
+++ src/modules/hcp/client/components/search-professional-hcp.component.js
@@ -37,7 +37,7 @@ const SearchProfessionalHcp = () => {
 
     const resetSearch = (props) => {
         setFormData({});
-        currentPage(1);
+        setCurrentPage(1);
         setSelectedOption([]);
         setUsers([]);
         props.resetForm();
