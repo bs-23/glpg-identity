7aec9c7a68fcfd9f8dca1c70126739c1ff7a683a
LabebaBS23 <labeba.tahsin@bs-23.net>
2020-12-11 12:50:33 +0600

    faq pagination bug category sort

--- src/modules/platform/faq/server/faq.controller.js
+++ src/modules/platform/faq/server/faq.controller.js
@@ -57,8 +57,7 @@ async function getFaqItems(req, res) {
         const page = req.query.page ? req.query.page - 1 : 0;
         if (page < 0) return res.status(404).send("page must be greater or equal 1");
 
-        // const limit = req.query.limit ? req.query.limit : 30;
-        const limit = 3;
+        const limit = req.query.limit ? req.query.limit : 30;
         const offset = page * limit;
 
         const category = req.query.category === 'null' || req.query.category === undefined ? null : req.query.category;
