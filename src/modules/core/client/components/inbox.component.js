e09f8c2fbea6c70126646a724402df77e9817d64
Nabila-Farzana <nabila.farzana@bs-23.net>
2020-12-23 19:39:48 +0600

    cdp_inbox with all required functinalities

--- src/modules/core/client/components/inbox.component.js
+++ src/modules/core/client/components/inbox.component.js
@@ -4,12 +4,13 @@ import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getHcpProfiles } from '../../../information/hcp/client/hcp.actions'; 
 import { isArray } from "lodash";
-
+import StatusupdateModal from './statusUpdateModal.component';
 
 export default function Inbox(){
   const dispatch = useDispatch();
   const [selectedTab, setSelectedTab] = useState('hcpaproval');
-
+  const [userToStatusUpdate, setuserToStatusUpdate] = useState({});
+  const [showModal, setShowModal] = useState(false);
   const getHcps=()=>{
     dispatch(getHcpProfiles(1, 'not_verified'));
   }
@@ -17,13 +18,12 @@ export default function Inbox(){
     getHcps();
   }, []);
   const hcps = useSelector(state => state.hcpReducer.hcps);
-  console.log("user Array:", hcps.users, "----", isArray(hcps.users));
-
+ 
   return(
     <div className="shadow-sm bg-white mb-3 cdp-inbox">
       <h5 className="p-3 cdp-text-primary font-weight-bold mb-0 d-flex justify-content-between cdp-inbox__header">
         CDP Queues / Tasks / Alerts
-                                <i onClick={() => setShow(true)} type="button" class="icon icon-expand faq-icon-expand faq__icon-toggle d-none d-lg-block"></i>
+        <i onClick={() => setShow(true)} type="button" class="icon icon-expand faq-icon-expand faq__icon-toggle d-none d-lg-block"></i>
         <i class="icon icon-minimize faq-icon-minimize  d-none faq__icon-toggle" type="button" onClick={() => setShow(false)}></i>
         <i className="icon icon-help faq__icon-help d-block d-lg-none"></i>
       </h5>
@@ -32,20 +32,21 @@ export default function Inbox(){
           <Tab eventKey="hcpaproval" title="HCP Approval">
             <div className="cdp-inbox__tab-detail">
               {hcps.users !== undefined && 
-                hcps.users.map(user => {
-                  <ul className="cdp-inbox__list p-0 m-0">
+                hcps.users.map((user, key) => <ul className="cdp-inbox__list p-0 m-0">
                     <li className="cdp-inbox__list-item d-flex justify-content-between  align-items-center border-bottom py-3 px-3">
                       <span className="cdp-inbox__list-item-col large cdp-text-primary font-weight-bold">{user.email}</span>
-                      <span className="cdp-inbox__list-item-col cdp-text-primary font-weight-bold px-3">{user.created_at}</span>
+                    <span className="cdp-inbox__list-item-col cdp-text-primary font-weight-bold px-3">{new Date(user.created_at).toLocaleDateString('en-GB', {
+                      day: 'numeric', month: 'short', year: 'numeric'
+                    }).replace(/ /g, ' ')}</span>
                       <span className="cdp-inbox__list-item-col">
-                        <button className="btn cdp-btn-secondary btn-sm text-white">Update Status</button>
+                      <button className="btn cdp-btn-secondary btn-sm text-white" onClick={() => { setShowModal(true)}}>Update Status</button>
                       </span>
                     </li>
+                  {showModal && <StatusupdateModal user={user} show={showModal} onHide={() => { setShowModal(false)}}/>}
                   </ul>
-                })
+                )
               }
-              
-              <NavLink to="/information/list" className="d-inline-block p-3 text-uppercase cdp-text-secondary active small font-weight-bold">
+              <NavLink to="/information/list?page=1&status=not_verified" className="d-inline-block p-3 text-uppercase cdp-text-secondary active small font-weight-bold">
                 More Pending
               </NavLink>
             </div>
