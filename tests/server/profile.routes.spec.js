afa1a24e03ba72b43409573d0f082e80fcfc6f08
= <Habibur.Rahman@brainstation23.com>
2021-01-01 13:29:32 +0600

    Write tests for profile routes

--- tests/server/profile.routes.spec.js
+++ tests/server/profile.routes.spec.js
@@ -4,6 +4,7 @@ const supertest = require('supertest');
 const specHelper = require(path.join(process.cwd(), 'jest/spec.helper'));
 const app = require(path.join(process.cwd(), 'src/config/server/lib/express'));
 
+const { defaultApplication } = specHelper;
 const { signCookie } = specHelper;
 const { consent: { demoConsent } } = specHelper;
 const { defaultAdmin, defaultUser } = specHelper.users;
@@ -21,23 +22,23 @@ beforeAll(async () => {
     request = supertest(appInstance);
 });
 
-describe('Role Routes', () => {
-    it('Should get roles', async () => {
+describe('Profile Routes', () => {
+    it('Should get profiles', async () => {
         const response = await request
-            .get(`/api/roles`)
+            .get(`/api/profiles`)
             .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`]);
 
         expect(response.statusCode).toBe(200);
         expect(response.res.headers['content-type']).toMatch('application/json');
     });
 
-    it('Should create and edit role', async () => {
+    it('Should create and edit profile', async () => {
         const response1 = await request
             .post(`/api/permissionSets`)
             .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
             .send({
-                title: "b",
-                description: "b",
+                title: "a",
+                description: "a",
                 countries: ["BE"],
                 serviceCategories: [serviceCategories[0].id],
                 applications: []
@@ -47,11 +48,11 @@ describe('Role Routes', () => {
         expect(response1.res.headers['content-type']).toMatch('application/json');
 
         const response2 = await request
-            .post(`/api/roles`)
+            .post(`/api/profiles`)
             .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
             .send({
-                title: "a",
-                description: "a",
+                title: "aa",
+                description: "aa",
                 permissionSets: [response1.body.id]
             });
 
@@ -59,11 +60,11 @@ describe('Role Routes', () => {
         expect(response2.res.headers['content-type']).toMatch('application/json');
 
         const response3 = await request
-            .put(`/api/roles/${response2.body.id}`)
+            .put(`/api/profiles/${response2.body.id}`)
             .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
             .send({
                 title: "a",
-                description: "aa",
+                description: "bb",
                 permissionSets: [response1.body.id]});
 
         expect(response3.statusCode).toBe(200);
