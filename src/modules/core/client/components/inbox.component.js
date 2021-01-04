474968921efe67d6f6fdcc66c853eb1ab72aa4d8
Nabila-Farzana <nabila.farzana@bs-23.net>
2021-01-04 17:53:26 +0600

    More pending link will be hidden if data length is less than 5

--- src/modules/core/client/components/inbox.component.js
+++ src/modules/core/client/components/inbox.component.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+//import { Tabs, Tab } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getHcpProfiles } from '../../../information/hcp/client/hcp.actions';
@@ -9,13 +10,12 @@ import { Tabs, TabList, Tab, PanelList, Panel } from 'react-tabtab';
 export default function Inbox(){
   const dispatch = useDispatch();
   const [show, setShow] = React.useState();
+  const [selectedTab, setSelectedTab] = useState('hcpaproval');
   const [showModal, setShowModal] = useState(false);
   const [modalId, setModalId] = useState(null);
-
   const getHcps=()=>{
       dispatch(getHcpProfiles('?page=1&status=not_verified&limit=5'));
   }
-
   let hcps = useSelector(state => state.hcpReducer.hcps);
   useEffect(() => {
     getHcps();
@@ -52,12 +52,13 @@ export default function Inbox(){
                                               <button className="btn cdp-btn-secondary btn-sm text-white" onClick={() => { setModalId(key); setShowModal(true) }}>Update Status</button>
                                           </span>
                                       </li>
-                                      {(showModal && modalId === key) && <StatusupdateModal user={user} show={showModal} onHide={() => { setShowModal(false) }} />}
+                                      {(showModal && modalId === key) && <StatusupdateModal user={user} limit={5} show={showModal} onHide={() => { setShowModal(false) }} status={'not_verified'}/>}
                                   </ul>
                                   )
                               }
                               {hcps.users !== undefined && hcps.users.length !== 0 ?
-                                  <NavLink to="/information/list?page=1&status=not_verified" className="d-inline-block p-3 text-uppercase cdp-text-secondary active small font-weight-bold">More Pending</NavLink>
+                                  <NavLink to="/information/list?page=1&status=not_verified" className="d-inline-block p-3 text-uppercase cdp-text-secondary active small font-weight-bold">
+                                      {hcps.users.length === 5 && 'More Pending' }</NavLink>
                                   : <h5 className="d-block py-5 px-2 text-uppercase cdp-text-secondary active text-center mb-0"><i className="far fa-folder-open mr-2"></i>No Data Found</h5>}
                           </div>
                       </Panel>
