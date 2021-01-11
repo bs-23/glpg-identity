b8f93b1c43144abf2b36c125341487d3ccb1cf06
= <Habibur.Rahman@brainstation23.com>
2021-01-11 17:08:08 +0600

    Add action functionality

--- src/modules/partner/manage-partners/client/components/wholeseller-business-partner-management.component.js
+++ src/modules/partner/manage-partners/client/components/wholeseller-business-partner-management.component.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Faq } from '../../../../platform';
 import Modal from 'react-bootstrap/Modal';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useSelector, useDispatch } from 'react-redux';
+import { useToasts } from 'react-toast-notifications';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import { NavLink } from 'react-router-dom';
+import { Faq } from '../../../../platform';
 
 const WholesellerBusinessPartnerManagement = () => {
+    const dispatch = useDispatch();
+    const { addToast } = useToasts();
+    const [showForm, setShowForm] = useState(false);
+    const [companyCodes, setCompanyCodes] = useState([{ id: Math.random(), company_code: '' }]);
+    const [showError, setShowError] = useState(false);
+    const [partnerRequestId, setPartnerRequestId] = useState(undefined);
+
     const [showFaq, setShowFaq] = useState(false);
     const handleCloseFaq = () => setShowFaq(false);
     const handleShowFaq = () => setShowFaq(true);
@@ -18,6 +28,62 @@ const WholesellerBusinessPartnerManagement = () => {
             procurement_contact: "daniel@gmail.com"
         }
     ];
+
+    const deleteRequest = (id) => {
+        dispatch(deletePartnerRequest(id)).then(() => {
+            addToast('Request Deleted', {
+                appearance: 'success',
+                autoDismiss: true
+            });
+        }).catch(error => {
+            addToast(error.response.data, {
+                appearance: 'error',
+                autoDismiss: true
+            });
+        });
+    }
+
+    const toggleForm = (id) => {
+        setPartnerRequestId(id);
+        setShowForm(!!id);
+    };
+
+    const handleChange = (e) => {
+        const newCompanyCodes = [...companyCodes];
+        const field = e.target.className.split(' ');
+        const companyCode = newCompanyCodes[e.target.dataset.id];
+        companyCode[field[1]] = e.target.value;
+        setCompanyCodes(newCompanyCodes);
+        setShowError(false);
+    }
+
+    const addNewCompanyCode = () => {
+        const newCompanyCodes = [...companyCodes, { id: Math.random(), company_code: '' }];
+        setCompanyCodes(newCompanyCodes);
+        setShowError(false);
+        setTimeout(() => {
+            const lastCompanyCode = document.getElementById(`company-code-${companyCodes.length + 1}`);
+            lastCompanyCode.scrollIntoView({ behavior: 'smooth' });
+        }, 50);
+    }
+
+    const getCompanyCodeFields = () => {
+        console.log(companyCodes);
+        return companyCodes.map((item, idx) => {
+            const companyCodeId = `company-code-${idx + 1}`;
+
+            return (<React.Fragment key={item.id}>
+                <div className="col-12 col-sm-6">
+                    <div className="form-group">
+                        <label className="font-weight-bold" htmlFor={companyCodeId}> {`Company Code ${idx+1}`} <span className="text-danger">*</span></label>
+                        <Field className="form-control company_code" type='text' value={item.company_code} onChange={(e) => handleChange(e)} data-id={idx} name={companyCodeId} id={companyCodeId}/>
+                        {showError && !item.company_code && <div class="invalid-feedback">This field must not be empty.</div>}
+                    </div>
+                </div>
+            </React.Fragment>
+            )
+        });
+    };
 
     return (
         <main className="app__content cdp-light-bg">
@@ -101,6 +167,109 @@ const WholesellerBusinessPartnerManagement = () => {
                     </div>
                 </div>
             </div>
+
+            <Modal dialogClassName="modal-90w modal-customize" centered show={showForm}  onHide={toggleForm}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Add Request
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        initialValues={{
+                            first_name: partnerRequestId && request ? request.first_name : '',
+                            last_name: partnerRequestId && request ? request.last_name : '',
+                            email: partnerRequestId && request ? request.email : '',
+                            procurement_contact: partnerRequestId && request ? request.procurement_contact : '',
+                            company_codes: [],
+                        }}
+                        displayName="PartnerRequestsForm"
+                        // validationSchema={partnerRequestSchema}
+                        enableReinitialize={true}
+                        onSubmit={(values, actions) => {
+                            values.company_codes = companyCodes.map(i => i.company_code);
+
+                            const validCompanyCodes = companyCodes.filter(item => item.company_code);
+                            if (companyCodes.length !== validCompanyCodes.length) {
+                                setShowError(true);
+                                return;
+                            }
+
+                            // if (partnerRequestId) {
+                            //     dispatch(updatePartnerRequest(partnerRequestId, values)).then(function () {
+                            //         toggleForm(null);
+                            //         addToast('Request updated successfully', {
+                            //             appearance: 'success',
+                            //             autoDismiss: true
+                            //         });
+                            //     }).catch(error => {
+                            //         addToast(error.response.data, {
+                            //             appearance: 'error',
+                            //             autoDismiss: true
+                            //         });
+                            //     });
+                            // } else {
+                            //     dispatch(createPartnerRequest(values)).then(() => {
+                            //         toggleForm(null);
+                            //         actions.resetForm();
+                            //         addToast('New Request Added', {
+                            //             appearance: 'success',
+                            //             autoDismiss: true
+                            //         });
+                            //     }).catch(error => {
+                            //         addToast(error.response.data, {
+                            //             appearance: 'error',
+                            //             autoDismiss: true
+                            //         });
+                            //     });
+                            // }
+
+                            actions.setSubmitting(false);
+                        }}
+                    >
+                        {formikProps => (
+                            <Form onSubmit={formikProps.handleSubmit}>
+                                <div className="row">
+                                    <div className="col-12 col-sm-12">
+                                        <div className="form-group">
+                                            <label className="font-weight-bold" htmlFor="first_name">First Name <span className="text-danger">*</span></label>
+                                            <Field className="form-control" type="text" name="first_name" />
+                                            <div className="invalid-feedback"><ErrorMessage name="first_name" /></div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label className="font-weight-bold" htmlFor="last_name">Last Name <span className="text-danger">*</span></label>
+                                            <Field className="form-control" type="text" name="last_name" />
+                                            <div className="invalid-feedback"><ErrorMessage name="last_name" /></div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label className="font-weight-bold" htmlFor="email">Email Address <span className="text-danger">*</span></label>
+                                            <Field className="form-control" type="text" name="email" />
+                                            <div className="invalid-feedback"><ErrorMessage name="email" /></div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label className="font-weight-bold" htmlFor="procurement_contact">Procurement Contact <span className="text-danger">*</span></label>
+                                            <Field className="form-control" type="text" name="procurement_contact" />
+                                            <div className="invalid-feedback"><ErrorMessage name="procurement_contact" /></div>
+                                        </div>
+
+                                        {getCompanyCodeFields()}
+
+                                        <div className="col-12">
+                                            <div className="form-group">
+                                                <label className="d-flex align-items-center cdp-text-primary hover-opacity" type="button" onClick={addNewCompanyCode}>
+                                                    <i className="fas fa-plus  fa-2x mr-3" ></i>
+                                                    <span className="h4 mb-0">Add Company Code</span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" disabled={showError} className="btn btn-block text-white cdp-btn-secondary mt-4 p-2" >Submit</button>
+                            </Form>
+                        )}
+                    </Formik>
+                </Modal.Body>
+            </Modal>
         </main >
 
     );
