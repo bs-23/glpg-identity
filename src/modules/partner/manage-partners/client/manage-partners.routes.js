5293a292c99392e3cf45080c5ab830637e3456cd
= <Habibur.Rahman@brainstation23.com>
2021-01-12 17:02:39 +0600

    Separate Hco and hcp and structure two services

--- src/modules/partner/manage-partners/client/manage-partners.routes.js
+++ src/modules/partner/manage-partners/client/manage-partners.routes.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Switch, useRouteMatch, Route } from "react-router-dom";
 import PrivateRoute from "../../../core/client/PrivateRoute";
 import BusinessPartnerManagement from "./components/business-partner-management.component";
-import Supplier from "./components/supplier.component";
+import HcpBusinessPartnerManagement from "./components/hcp-business-partner-management.component";
+import HcoBusinessPartnerManagement from "./components/hco-business-partner-management.component";
 import VendorManagement from "./components/vendor-management.component";
 import VendorBusinessPartnerManagement from "./components/vendor-business-partner-management.component";
 import WholesalerBusinessPartnerManagement from "./components/wholesaler-business-partner-management.component";
@@ -14,9 +15,10 @@ export default function ManagePartnersRoutes() {
     return (
         <Switch>
             <PrivateRoute exact path={path} component={BusinessPartnerManagement} />
+            <PrivateRoute path={`${path}/requests/hcos`} component={HcoBusinessPartnerManagement} />
+            <PrivateRoute path={`${path}/requests/hcps`} component={HcpBusinessPartnerManagement} />
             <PrivateRoute path={`${path}/requests/vendors`} component={VendorBusinessPartnerManagement} />
             <PrivateRoute path={`${path}/requests/wholesalers`} component={WholesalerBusinessPartnerManagement} />
-            <PrivateRoute path={`${path}/requests`} component={Supplier} />
             <PrivateRoute path={`${path}/vendor-management/vendors`} component={VendorManagement} />
             <PrivateRoute path={`${path}/vendor-management/hcp`} component={VendorManagement} />
             <PrivateRoute path={`${path}/vendor-management/hco`} component={VendorManagement} />
