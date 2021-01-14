ba1f2a61285246e8320e6dfefcd2c103d496c6ec
bshabib <Habibur.Rahman@brainstation23.com>
2021-01-14 21:27:30 +0600

    Rename germany to german and nederlands to dutch

--- src/modules/partner/manage-requests/client/components/hcp-partner-requests.component.js
+++ src/modules/partner/manage-requests/client/components/hcp-partner-requests.component.js
@@ -19,8 +19,8 @@ const HcpPartnerRequests = () => {
     const countryLanguages = [
         { language_name: 'English', language_code: 'en' },
         { language_name: 'French', language_code: 'fr' },
-        { language_name: 'Germany', language_code: 'de' },
-        { language_name: 'Netherlands', language_code: 'nl' }
+        { language_name: 'German', language_code: 'de' },
+        { language_name: 'Dutch', language_code: 'nl' }
     ];
     const partnerTypes = ['SUPL', 'CUST', 'HCP', 'HCO', 'POR', 'ZVST'];
     const [requestToDelete, setRequestToDelete] = useState(null);
