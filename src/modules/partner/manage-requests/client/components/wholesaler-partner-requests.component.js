0c4e73965a4ed7b23c1cd9a269ffe08148248d47
Ruhul Amin <ruhul100@gmail.com>
2021-01-14 21:22:37 +0600

    Wholesaler creation form styling

--- src/modules/partner/manage-requests/client/components/wholesaler-partner-requests.component.js
+++ src/modules/partner/manage-requests/client/components/wholesaler-partner-requests.component.js
@@ -352,6 +352,8 @@ const WholesalerPartnerRequests = () => {
                                             <Field className="form-control" type="text" name="partner_type" />
                                             <div className="invalid-feedback"><ErrorMessage name="partner_type" /></div>
                                         </div>
+                                    </div>
+                                    <div className="col-12 col-sm-6 col-lg-4">
                                         <div className="form-group">
                                             <label className="font-weight-bold" htmlFor="email">Email Address <span className="text-danger">*</span></label>
                                             <Field className="form-control" type="text" name="email" />
@@ -364,17 +366,19 @@ const WholesalerPartnerRequests = () => {
                                             <Field className="form-control" type="text" name="procurement_contact" />
                                             <div className="invalid-feedback"><ErrorMessage name="procurement_contact" /></div>
                                         </div>
-
-                                        {getCompanyCodeFields()}
-
-                                        <div className="col-12">
-                                            <div className="form-group">
-                                                <label className="d-flex align-items-center cdp-text-primary hover-opacity" type="button" onClick={addNewCompanyCode}>
-                                                    <i className="fas fa-plus  fa-2x mr-3" ></i>
-                                                    <span className="h4 mb-0">Add Company Code</span>
-                                                </label>
+                                    </div>
+                                    <div className="col-12">
+                                        <div className="row py-3">
+                                            {getCompanyCodeFields()}
+                                            <div className="col-12 col-sm-6 col-lg-4">
+                                                <label>&#160;</label>
+                                                <div className="form-group">
+                                                    <div className="d-flex align-items-center hover-opacity" type="button" onClick={addNewCompanyCode}>
+                                                        <i className="fas fa-plus cdp-text-secondary mr-2" ></i>
+                                                        <span className="cdp-text-secondary mb-0">Add Company Code</span>
+                                                    </div>
+                                                </div>
                                             </div>
-
                                         </div>
                                     </div>
                                     <div className="col-12 col-sm-6 col-lg-4">
