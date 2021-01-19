d1716ce2b2cc86edf76df520f245d9e6b79376a6
Ruhul Amin <ruhul100@gmail.com>
2021-01-19 12:16:56 +0600

    Breadcrumb responsive for SAP pages

--- src/modules/partner/manage-requests/client/components/vendor-partner-requests.component.js
+++ src/modules/partner/manage-requests/client/components/vendor-partner-requests.component.js
@@ -159,13 +159,23 @@ const VendorPartnerRequests = () => {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 px-0">
-                        <nav aria-label="breadcrumb">
-                            <ol className="breadcrumb rounded-0">
+                        <nav className="breadcrumb justify-content-between align-items-center" aria-label="breadcrumb">
+                            <ol className="rounded-0 m-0 p-0 d-none d-sm-flex">
                                 <li className="breadcrumb-item"><NavLink to="/">Dashboard</NavLink></li>
                                 <li className="breadcrumb-item"><NavLink to="/business-partner">Business Partner Management</NavLink></li>
                                 <li className="breadcrumb-item active"><span>Business Partner Requests</span></li>
-                                <li className="ml-auto mr-3"><i type="button" onClick={handleShowFaq} className="icon icon-help icon-2x cdp-text-secondary"></i></li>
                             </ol>
+                            <Dropdown className="dropdown-customize breadcrumb__dropdown d-block d-sm-none ml-2">
+                                <Dropdown.Toggle variant="" className="cdp-btn-outline-primary dropdown-toggle btn d-flex align-items-center border-0">
+                                    <i className="fas fa-arrow-left mr-2"></i> Back
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item className="px-2" href="/"><i className="fas fa-link mr-2"></i> Dashboard</Dropdown.Item>
+                                    <Dropdown.Item className="px-2" href="/business-partner"><i className="fas fa-link mr-2"></i> Business Partner Management</Dropdown.Item>
+                                    <Dropdown.Item className="px-2" active><i className="fas fa-link mr-2"></i> Business Partner Requests</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            <span className="ml-auto mr-3"><i type="button" onClick={handleShowFaq} className="icon icon-help breadcrumb__faq-icon cdp-text-secondary"></i></span>
                         </nav>
                         <Modal show={showFaq} onHide={handleCloseFaq} size="lg" centered>
                             <Modal.Header closeButton>
