85bc02758577c8c5c8b7596ad0b4179e3d202cd2
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-02-01 11:22:02 +0600

    Guard Consent Routes

--- src/modules/privacy/consent-performance/server/consent-performance.routes.js
+++ src/modules/privacy/consent-performance/server/consent-performance.routes.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const controller = require('./consent-performance.controller');
 const { CDPAuthStrategy } = require(path.join(process.cwd(), 'src/modules/platform/user/server/user-authentication.middleware.js'));
+const { Modules } = require(path.join(process.cwd(), 'src/modules/core/server/authorization/authorization.constants'));
+const { ModuleGuard } = require(path.join(process.cwd(), 'src/modules/core/server/authorization/authorization.middleware'));
 
 module.exports = app => {
     app.route('/api/cdp-consent-performance-report')
-        .get(CDPAuthStrategy, controller.getCdpConsentsReport);
+        .get(CDPAuthStrategy, ModuleGuard(Modules.PRIVACY.value), controller.getCdpConsentsReport);
 
     app.route('/api/veeva-consent-performance-report')
-        .get(CDPAuthStrategy, controller.getVeevaConsentsReport);
+        .get(CDPAuthStrategy, ModuleGuard(Modules.PRIVACY.value), controller.getVeevaConsentsReport);
 };
