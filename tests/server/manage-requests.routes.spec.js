b6ec255bc7af8e198a461cccb6f49d4847fd3bbb
bshabib <Habibur.Rahman@brainstation23.com>
2021-03-03 20:07:31 +0600

    Add more tests for manage requests.

--- tests/server/manage-requests.routes.spec.js
+++ tests/server/manage-requests.routes.spec.js
@@ -31,8 +31,8 @@ describe('Manage Requests Routes', () => {
         expect(response.res.headers['content-type']).toMatch('application/json');
     });
 
-    it('Should create partner request', async () => {
-        const response = await request
+    it('Should create, update and delete partner request', async () => {
+        const response1 = await request
             .post(`/api/partner-requests`)
             .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
             .send({
@@ -52,7 +52,44 @@ describe('Manage Requests Routes', () => {
                 workplace_type: "healthcare_org"
             });
 
-        expect(response.statusCode).toBe(200);
-        expect(response.res.headers['content-type']).toMatch('application/json');
+        expect(response1.statusCode).toBe(200);
+        expect(response1.res.headers['content-type']).toMatch('application/json');
+
+        const response2 = await request
+            .get(`/api/partner-requests/${response1.body.id}`)
+            .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`]);
+
+        expect(response2.statusCode).toBe(200);
+        expect(response2.res.headers['content-type']).toMatch('application/json');
+
+        const response3 = await request
+            .put(`/api/partner-requests/${response1.body.id}`)
+            .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
+            .send({
+                entity_type: "hco",
+                first_name: "bb",
+                last_name: "aa",
+                email: "test@gmail.com",
+                confirm_email: "test@gmail.com",
+                procurement_contact: "test@gmail.com",
+                country_iso2: "BE",
+                language: "fr",
+                mdr_id: "test_mdr_id",
+                onekey_id: "",
+                specialty: "SP.WBE.21",
+                uuid: "test_uuid",
+                workplace_name: "test_workplace_name",
+                workplace_type: "healthcare_org"
+            })
+
+        expect(response3.statusCode).toBe(200);
+        expect(response3.res.headers['content-type']).toMatch('application/json');
+
+        const response4 = await request
+            .delete(`/api/partner-requests/${response1.body.id}`)
+            .set('Cookie', [`access_token=s:${signCookie(defaultAdmin.access_token)}`])
+
+        expect(response4.statusCode).toBe(200);
+        expect(response4.res.headers['content-type']).toMatch('application/json');
     });
 });
