bf798ae358370257c977cfc0bf8a3cfff415d0e3
Ruhul Amin <ruhul100@gmail.com>
2021-03-10 17:18:42 +0600

    Statistic styling

--- src/modules/core/client/statistics/statistics.component.js
+++ src/modules/core/client/statistics/statistics.component.js
@@ -12,39 +12,38 @@ export default function HotStatistic() {
     }, []);
 
     return (
-        <div className={`faq shadow-sm bg-white ${show ? "cdp-inbox__expand" : ""}`}>
+        <div className={`hot-statistics shadow-sm bg-white ${show ? "cdp-inbox__expand" : ""}`}>
             <h5 className="p-3 cdp-text-primary font-weight-bold mb-0 d-flex justify-content-between cdp-inbox__header">
                 Hot Statistics
                 <i onClick={() => setShow(true)} class="icon icon-expand cdp-inbox__icon-expand cdp-inbox__icon-toggle d-none d-lg-block cursor-pointer"></i>
                 <i class="icon icon-minimize cdp-inbox__icon-minimize cdp-inbox__icon-toggle cursor-pointer" onClick={() => setShow(false)}></i>
-                <i className="far fa-bell cdp-inbox__icon-bell d-block d-lg-none"></i>
+                <i className="fas fa-chart-line d-block d-lg-none"></i>
             </h5>
-
-            <div className="container">
+            <div className="p-3 shadow-sm hot-statistics__box-wrap">
                 <div className="row">
-                    <div className="col-4 hot-statistics__box">
-                        <div className="hot-statistics__title">Total Users</div>
+                    <div className="col-6 col-md-4 hot-statistics__box pb-4">
+                        <div className="hot-statistics__title pb-3">Total Users</div>
                         <div className="hot-statistics__amount">{statistics.users_count}</div>
                     </div>
-                    <div className="col-4 hot-statistics__box">
-                        <div className="hot-statistics__title">Total HCP Users</div>
+                    <div className="col-6 col-md-4 hot-statistics__box pb-4">
+                        <div className="hot-statistics__title pb-3">Total HCP Users</div>
                         <div className="hot-statistics__amount">{statistics.hcps_count}</div>
                     </div>
-                    <div className="col-4 hot-statistics__box">
-                        <div className="hot-statistics__title">Total Consents</div>
+                    <div className="col-6 col-md-4 hot-statistics__box pb-4">
+                        <div className="hot-statistics__title pb-3">Total Consents</div>
                         <div className="hot-statistics__amount">{statistics.consents_count}</div>
                     </div>
-                    <div className="col-4 hot-statistics__box">
-                        <div className="hot-statistics__title"> Total Captured Consents</div>
+                    <div className="col-6 col-md-4 hot-statistics__box pb-4">
+                        <div className="hot-statistics__title pb-3"> Total Captured Consents</div>
                         <div className="hot-statistics__amount">{statistics.captured_consents_count}</div>
                     </div>
-                    <div className="col-4 hot-statistics__box">
-                        <div className="hot-statistics__title"> Total Campaigns</div>
+                    <div className="col-6 col-md-4 hot-statistics__box pb-4">
+                        <div className="hot-statistics__title pb-3"> Total Campaigns</div>
                         <div className="hot-statistics__amount">0</div>
                     </div>
-                    <div className="col-4 hot-statistics__box"></div>
                 </div>
             </div>
+            
         </div>
     )
 }
