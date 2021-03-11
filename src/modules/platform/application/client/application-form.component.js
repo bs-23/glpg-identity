e6d0b6b76e40a2e773904add0777ee67edbdf2a0
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-03-11 18:56:05 +0600

    Add logo _url to metadataOptions in application form

--- src/modules/platform/application/client/application-form.component.js
+++ src/modules/platform/application/client/application-form.component.js
@@ -33,7 +33,8 @@ const ApplicationForm = ({ onSuccess, isEditing, applicationId }) => {
         ],
         "hcp-portal": [
             "cache_clearing_url",
-            "approve_user_path"
+            "approve_user_path",
+            "logo_url"
         ]
     }
 
