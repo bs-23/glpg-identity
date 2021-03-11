a6bb2d32f27fcf83ead85c57a1c4fd26bd4c6d4f
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-03-11 12:35:55 +0600

    Fix import error

--- src/modules/core/client/reducers.js
+++ src/modules/core/client/reducers.js
@@ -5,7 +5,7 @@ import { consentReducer, consentCategoryReducer, consentCountryReducer, consentP
 import { faqReducer, userReducer, profileReducer, roleReducer, permissionSetReducer } from '../../platform';
 import { manageRequestsReducer, managePartnerReducer } from '../../partner';
 import { applicationReducer } from '../../platform';
-import localizationsReducer from '../../core/client/localizations/localizations.reducer';
+import localizationsReducer from '../../core/client/localization/localization.reducer';
 import clinicalTrialsReducer from '../../clinical-trials/client/components/clinical-trials.reducer';
 
 export default combineReducers({
