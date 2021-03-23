8472246b574b59fa3314ac16dfe3f03ccee434e3
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-03-23 09:55:31 +0600

    Statistics Component

--- src/modules/core/client/statistics/statistics.actions.js
+++ src/modules/core/client/statistics/statistics.actions.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import Types from './statistics.types';
 
-export function getStatistics() {
+export function getStatistics(query = '') {
     return {
         type: Types.GET_STATISTICS,
         payload: axios({
             method: 'get',
-            url: '/api/statistics'
+            url: `/api/statistics${query}`
         })
     };
 }
