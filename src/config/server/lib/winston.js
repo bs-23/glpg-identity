f0896ca9984add8fc1ce9514850f7f5db1a49856
faisal-nstu <faisal.amin@bs-23.com>
2021-03-24 19:31:16 +0600

    activate winston

--- src/config/server/lib/winston.js
+++ src/config/server/lib/winston.js
@@ -1,26 +1,14 @@
 const winston = require('winston');
 const newrelicFormatter = require('@newrelic/winston-enricher');
 
-// const logger = winston.createLogger({
-//     transports: [
-//         new winston.transports.Console({
-//             format: winston.format.combine(
-//                 newrelicFormatter()
-//             )
-//         })
-//     ]
-// });
-
-const logger = {
-    log: function (err) {
-        console.log('**************************** ', err);
-    },
-    info: function (err) {
-        console.log('============================ ', err);
-    },
-    error: function (err) {
-        console.log(':::::::::::::::::::::::::::: ', err);
-    }
-};
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                newrelicFormatter()
+            )
+        })
+    ]
+});
 
 module.exports = logger;
