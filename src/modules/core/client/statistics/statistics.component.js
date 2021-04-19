d34df91ed5ec2cd7744ab8c22b57ccad9733cce1
Nabila-Farzana <nabila.farzana@bs-23.net>
2021-04-19 10:57:04 +0600

    Warning resolved in statistics component

--- src/modules/core/client/statistics/statistics.component.js
+++ src/modules/core/client/statistics/statistics.component.js
@@ -16,7 +16,7 @@ export default function HotStatistic() {
 
     let getCountryOptions = countries.filter(c => permittedCountries.map(c => c.toLowerCase()).includes(c.country_iso2.toLowerCase())).map(c => ({ value: c.country_iso2.toLowerCase(), label: c.countryname, checked: true}));
 
-    const getSelectedCountries = (country,index) => {
+    const getSelectedCountries = (country) => {
         countryOptions.map(countryOption => {
           if(countryOption.value === country.value){
               countryOption.checked = !countryOption.checked;
@@ -92,7 +92,7 @@ export default function HotStatistic() {
                     <div className="col-12 mb-3">
                         <div className="bg-white border rounded p-2">
                             {countryOptions.map((country, key) => {
-                                return <span title={country.checked ? "Click to deselect" : "Click to select"} className={country.checked ? "badge hot-statistics__badge-selected text-white mr-2 mb-1 font-weight-normal cursor-pointer hot-statistics__badge" : "badge mr-2 mb-1 font-weight-normal cursor-pointer hot-statistics__badge text-dark"} onClick={(e) => { getSelectedCountries(country, key) }}>{country.label}
+                                return <span key={key} title={country.checked ? "Click to deselect" : "Click to select"} className={country.checked ? "badge hot-statistics__badge-selected text-white mr-2 mb-1 font-weight-normal cursor-pointer hot-statistics__badge" : "badge mr-2 mb-1 font-weight-normal cursor-pointer hot-statistics__badge text-dark"} onClick={() => { getSelectedCountries(country) }}>{country.label}
                                 </span>
                             })}
                         </div>
